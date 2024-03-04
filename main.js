ab79a45929d04d3d9d28657cff9b8823aa155979
LianaSh <lilishg91@gmail.com>
2024-03-04 22:17:44 +0500

    Сделала модальное окно в JS

--- main.js
+++ main.js
@@ -1,2 +1,9 @@
 "use strict";
 
+const modal = document.querySelector('#modal');
+const card = document.querySelector('#card');
+
+modal.addEventListener("click", function() {
+    console.log("Click!");
+    card.classList.toggle('dn');
+});
